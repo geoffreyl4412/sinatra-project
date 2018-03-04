cab7b75ac692dbbc638e1c4ccfda9f7a5730163d
geoffreyl4412 <geoffreyl4412@hstat.org>
2018-03-04 20:42:37 +0000

    grab info from array

--- models/model.rb
+++ models/model.rb
@@ -1,9 +1,25 @@
-class Ghibli
-    
-   attr_
-    
-    def initialize(title)
-        @title = title
-    end
+# https://ghibliapi.herokuapp.com/films
+# hash of each film
 
-end
+require 'net/http'
+require 'json'
+ 
+url = 'https://ghibliapi.herokuapp.com/films'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+film = JSON.parse(response)
+
+puts film[0]["title"]
+puts film[0]["description"]
+puts film[0]["director"]
+puts film[0]["producer"]
+
+# class Ghibli
+    
+#   attr_
+    
+#     def initialize(title)
+#         @title = title
+#     end
+
+# end
