7edece949fd97bfc285193d18122788e3d7da505
stephenw6888 <stephenw6888@hstat.org>
2018-03-15 19:01:09 +0000

    fixed app_contr

--- application_controller.rb
+++ application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < Sinatra::Base
   
   post '/result' do
     
-    film = params[:film]
-    @user_film_result = get_film_info(film)
+    film_title = params[:film_title]
+    @user_film_result = title_to_id(film_title)
     erb :result
     
   end
