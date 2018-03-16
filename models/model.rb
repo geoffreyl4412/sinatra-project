fcf4cf426d3840db5823149f49a4b45ce9d79ed5
simiek <simeonk3817@hstat.org>
2018-03-16 13:13:15 +0000

    Add stephens comments

--- models/model.rb
+++ models/model.rb
@@ -2,23 +2,26 @@ require 'net/http'
 require 'json'
 
 def get_film_info(info_type)
-  url = 'https://ghibliapi.herokuapp.com/films/cd3d059c-09f4-4ff3-8d63-bc765a5184fa' #A certain link being grab from inside the API link
+  url = 'https://ghibliapi.herokuapp.com/films/cd3d059c-09f4-4ff3-8d63-bc765a5184fa'
   uri = URI(url)
   response = Net::HTTP.get(uri)
   film = JSON.parse(response)
   
-  puts film["title"] #This is the title that we want to see on the result
-  puts film["description"] #This is the description that we want to see on the result
-  puts film["director"] #This is the director that we want to see on the result
-  puts film["producer"] #This is the producer that we want to see on the result
-  puts film["release_date"] #This is the release_date that we want to see on the result
-  puts film["rt_score"] #This is the rt_score that we want to see on the result
+  film["#{info_type}"] #These are the results that we want to see on the result
+  
 end
 
+# puts get_film_info("description")
 # class Ghibli
-#   attr_reader :film_name
+#   attr_reader :film_name, :result
 #   def initialize(film_name)
 #     @film_name = film_name
+#   end
+#   def get_name
+#     begin 
+    
+#     rescue
+#     @result = ["sorry that film isnt avaliable"]
 #   end
 # end 
 
